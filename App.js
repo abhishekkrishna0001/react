5d01828d2e4113743ea748f81ede3e0249061079
Abhishek <abhishekkrishna0001@gmail.com>
2024-12-17 11:37:54 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-//const heading=React.createElement("h1",{id:"heading",xyz:"abc"},"Hello World from React!");
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent=React.createElement("div",{id:"parent"},
     [React.createElement("div",{id:"child1"},

c5686e61105867f17c8f0359d872d80c9775fffe
Abhishek <abhishekkrishna0001@gmail.com>
2025-01-29 12:34:10 +0530

    ep-06

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,83 +1,55 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
+//import resList from "../utils/mockData";
+import Shimmer from "./Shimmer";
 
-import { useState } from "react";
-
-// let listOfRestaurant=[
-//       {
-//         "info": {
-//           "id": "699427",
-//           "name": "Domino's Pizza",
-//           "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/11/29/d3f2568c-7049-41ef-8479-533129037f29_699427.jpg",
-//           "costForTwo": "₹400 for two",
-//           "cuisines": [
-//             "Pizzas",
-//             "Italian",
-//             "Pastas",
-//             "Desserts"
-//           ],
-//           "avgRating": 4.4,
-//           "sla": {
-//             "deliveryTime": 25
-//           }
-//         }
-//       },
-//       {
-//         "info": {
-//           "id": "699428",
-//           "name": "KFC",
-//           "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/11/29/d3f2568c-7049-41ef-8479-533129037f29_699427.jpg",
-//           "costForTwo": "₹400 for two",
-//           "cuisines": [
-//             "Pizzas",
-//             "Italian",
-//             "Pastas",
-//             "Desserts"
-//           ],
-//           "avgRating": 3.8,
-//           "sla": {
-//             "deliveryTime": 25
-//           }
-//         }
-//       },
-//       {
-//         "info": {
-//           "id": "699429",
-//           "name": "Mcd",
-//           "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/11/29/d3f2568c-7049-41ef-8479-533129037f29_699427.jpg",
-//           "costForTwo": "₹400 for two",
-//           "cuisines": [
-//             "Pizzas",
-//             "Italian",
-//             "Pastas",
-//             "Desserts"
-//           ],
-//           "avgRating": 4.2,
-//           "sla": {
-//             "deliveryTime": 25
-//           }
-//         }
-//       }
-// ];
+import { useEffect, useState } from "react";
 
 const Body=() => {
-    //const [listOfRestaurants, setListOfRestaurants]=useState(resList);
-    const arr=useState(resList);
-    //const [listOfRestaurants, setListOfRestaurants]=arr;
-    const listOfRestaurants= arr[0];
-    const setListOfRestaurants=arr[1];
-    return(
+
+    const [listOfRestaurants, setListOfRestaurants]=useState([]);
+    const [filteredListOfRestaurants, setFilteredListOfRestaurants] = useState([]);
+
+    const [searchText,setSearchText]=useState("");
+
+    const fetchData = async () => {
+        const data = await fetch("https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9817001&lng=77.62841519999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilteredListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    };
+    
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+
+    return listOfRestaurants.length===0 ? (<Shimmer />) : (
         <div className="body">
             <div className="filter">
+                <div className="search">
+                    <input type="text" className="search-box" value={searchText} onChange={(e) => {
+                        setSearchText(e.target.value);
+                    }}/>
+                    <button className="search-btn" onClick={() => {
+                        const filteredResList=listOfRestaurants.filter(
+                            (res) => res.info.name.toLowerCase().includes(searchText.toLowerCase())
+                        );
+                        setFilteredListOfRestaurants(filteredResList);
+                    }}
+                    >Search</button>
+                </div>
+                <div className="filter-top">
                 <button className="filter-btn" onClick={() => 
                         {
                             const filteredList=listOfRestaurants.filter((res) => res.info.avgRating>4.5);
-                            setListOfRestaurants(filteredList);
+                            setFilteredListOfRestaurants(filteredList);
                         }
                     }>Top Rated Restaurants</button>
+                </div>
             </div>
             <div className="res-container">
-                {listOfRestaurants.map((restaurant) => (
+                {filteredListOfRestaurants.map((restaurant) => (
                     <RestaurantCard key={restaurant.info.id} resData={restaurant} />
                 )
                 )}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import LOGO_URL from "../utils/constants";
 
 
 const Header=() => {
+    const [btnName, setBtnName] = useState("Login");
+
     return(
         <div className="header">
-            <div className="logo-container">
+            <div className = "logo-container">
                 <img 
-                className="logo" src={LOGO_URL}
+                className = "logo" src={LOGO_URL}
                 />
             </div>
             <div className="nav-items">
@@ -15,6 +18,9 @@ const Header=() => {
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    <button className="login" onClick={() =>{
+                        btnName==="Login" ? setBtnName("Logout") : setBtnName("Login");
+                    } }>{btnName}</button>
                 </ul>
             </div>
         </div>

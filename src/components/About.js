3787ae3b64ca07f9ab66a83f57d88020c7ee8122
Abhishek <abhishekkrishna0001@gmail.com>
2025-02-05 10:17:39 +0530

    ep-08

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,13 @@
+import User from "./user";
+import UserClass from "./UserClass";
+
 const About = () => {
     return (
         <div>
             <h1>About us page</h1>
             <h2>This is Namaste React web series</h2>
+            <User name={"Abhishek(function)"}/>
+            <UserClass name={"Abhishek(class)"}/>
         </div>
     );
 };
---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+const User = (props) => {
+    return (
+        <div className="user-card">
+            <h2>Name: </h2>
+            <h3>Location: Bengaluru</h3>
+            <h4>contact: @thyabhi</h4>
+        </div>
+    );
+};
+
+export default User;
